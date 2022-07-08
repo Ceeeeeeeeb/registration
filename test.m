94f85b2a13ef795d5bbfe1c8c15306f3f5aad099
Ceeeeeeeeb <1121136120@qq.com>
2022-07-08 10:01:28 +0800

    Signed-off-by: Ceeeeeeeeb <1121136120@qq.com>

--- test.m
+++ test.m
@@ -7,3 +7,10 @@ close all
 im = double(imread("./data/mask.tif"));
 im1 = double(imread("./data/contrast.tif"));
 
+level = 1;
+
+while(level<4)
+    % divide the source and target image into subimages
+    
+
+end
